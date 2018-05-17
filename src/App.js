423174b160c6d62e731791f377e66c5cd5ef3083
neverc <root@neverc.cn>
2018-05-17 16:14:43 +0800

    add 条件编译

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom';
 import { DatePicker, LocaleProvider, Button, message, notification } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
+
+let zhCN = null;
+if (process.env.NODE_ENV === 'production') {
+    var antd = require('antd'); // 生产环境
+    zhCN = antd.locales.zh_CN;
+} else {
+    zhCN = require('antd/lib/locale-provider/zh_CN'); // 开发环境
+}
 
 export default class App extends Component {
     render() {
