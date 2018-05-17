c0f110ce7c5b0c2ef41ea2335199c8d9097046d7
neverc <root@neverc.cn>
2018-05-17 10:58:29 +0800

    optimized code

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import zhCN from 'antd/lib/locale-provider/zh_CN';
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <LocaleProvider locale={zhCN}>
-                    <div>
-                        <DatePicker />
-                        <Button type="primary" onClick={() => { message.info('message.info()'); }}>message</Button>
-                        <Button type="primary" onClick={() => { notification.open({ message: 'notification.message', description: 'notification.description' }); }}>notification</Button>
-                    </div>
-                </LocaleProvider>
-            </div>
+            <LocaleProvider locale={zhCN}>
+                <div>
+                    <DatePicker />
+                    <Button type="primary" onClick={() => { message.info('message.info()'); }}>message</Button>
+                    <Button type="primary" onClick={() => { notification.open({ message: 'notification.message', description: 'notification.description' }); }}>notification</Button>
+                </div>
+            </LocaleProvider>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { render } from 'react-dom';
 import App from './App'
 
+var root = document.createElement('div');
+root.id = 'root';
+document.body.insertBefore(root, document.body.firstChild);
+
 render(
     <App />,
-    document.getElementById('root')
+    root
 );
