ac83bdf737999c5e1f5922fd8a6ceccfd12a553d
Never、C <caolin@never.cn>
2018-05-19 22:10:17 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom';
 import DatePicker from './AntDatePicker'
 // import Clock from './Clock'
+import './App.js'
 
 var root = document.createElement('div');
 root.id = 'root';
